06207b8b600d4377905b810bb49e3e37f2de1698
bllamond <vinaymalik1729@gmail.com>
2024-01-04 20:36:15 +0530

    rtk setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React ,{ useState , useEffect } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login , logout} from './store/authSlice'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>Hello blog webapp</h1>
-    </>
-  )
+
+  const [loading , setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData)
+      {
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally( () => setLoading(false))
+  },[])
+
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        < Header />
+        <main>
+        {/* Hi<Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
