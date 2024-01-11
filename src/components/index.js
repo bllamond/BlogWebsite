9d4e57d6d81a8a2f29cdd2d348a91dc233d2bcc9
bllamond <vinaymalik1729@gmail.com>
2024-01-11 16:49:21 +0530

    components and react hook form

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,12 @@
 import Header from "./header/header";
 import Footer from "./footer/footer";
-
-
+import Container from './container/Container'
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
